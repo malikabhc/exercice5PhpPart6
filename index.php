e1efd611ffdd18c8cda2fcf399103f6727713a88
Malikabhc <malika.bouhcine@gmail.com>
2018-07-31 15:23:37 +0200

    exercice5-6

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 4/6</title>
+  </head>
+  <body>
+    <!-- exo05-6/?week=12 -->
+      <?php
+        if (isset($_GET['week'])) {
+        echo $_GET['week'];
+      }
+      ?>
+  </body>
+</html>
